1862d883a9891f69552b96ea4d5e79f10ab7a13a
Lily <lily.nelsen@mymail.champlain.edu>
2024-09-08 14:23:41 -0400

    Final commit for Powershell Basics Lab

--- Scripts/lab_1.9.ps1
+++ Scripts/lab_1.9.ps1
@@ -1,4 +1,5 @@
 ﻿# Without changing directory, find every .csv file and change it to a .log file.
+
 $files=Get-ChildItem -Recurse -File
-$files | Where-Object - {$_.Extension -creplace '.csv', '.log'}
-Get-
+$files | Rename-Item -NewName {$_.Name -replace '.csv', '.log'}
+Get-ChildItem -Recurse -File
