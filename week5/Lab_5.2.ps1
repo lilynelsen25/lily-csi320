bac11ab000f98173f94549bc5d1965bf5109966b
Lily <lily.nelsen@mymail.champlain.edu>
2024-10-03 15:42:08 -0400

    Final push for week 5

---
+++ week5/Lab_5.2.ps1
@@ -0,0 +1,47 @@
+﻿function gatherClasses(){
+
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.19/Courses.html
+
+# Get all the tr elements of HTML document
+$trs=$page.ParsedHtml.body.getElementsByTagName("tr")
+
+# Empty array to hold results.
+$FullTable = @()
+for($i=1; $i -lt $trs.length; $i++){
+
+    # Get every td element of current tr element.
+    $tds = $trs[$i].getElementsByTagName("td")
+
+    # Want to separate start time and end time from one end field.
+    $Times = $tds[5].innerText.split("-")
+
+    $FullTable += [psCustomObject]@{"Class Code" = $tds[0].innerText; `
+                                    "Title" = $tds[1].innerText; `
+                                    "Days" = $tds[4].innerText; `
+                                    "Time Start" = $Times[0]; `
+                                    "Time End" = $Times[1]; `
+                                    "Instructor" = $tds[6].innerText; `
+                                    "Location" = $tds[9].innerText; `
+                                }
+} # end of for loop.
+return $FullTable
+}
+
+function daysTranslator($FullTable){
+
+#Go over every record in the table
+for($i=0; $i -lt $FullTable.length; $i++){
+
+    $Days = @()
+
+    if($FullTable[$i].Days -ilike "*M*"){ $Days += "Monday" }
+    if($FullTable[$i].Days -ilike "*T[T,W,F]*"){ $Days += "Tuesday" }
+    if($FullTable[$i].Days -ilike "*W*"){ $Days += "Wednesday" }
+    if($FullTable[$i].Days -ilike "*TH*"){ $Days += "Thursday" }
+    if($FullTable[$i].Days -ilike "*F*"){ $Days += "Friday" }
+
+    $FullTable[$i].Days = $Days
+}
+
+return $FullTable
+}
---
+++ week5/Lab_5.3.ps1
@@ -0,0 +1,25 @@
+﻿. (Join-Path $PSScriptRoot Lab_5.2.ps1)
+
+ $ClassTable = gatherClasses
+
+ $ClassDays = daysTranslator $ClassTable
+
+ # $ClassTable | Sort-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End" | `
+               # Where{ $_."Instructor" -ilike "Furkan Paligu" }
+
+ # $ClassTable | Where { ($_.Location -ilike "JOYC 310") -and ($_.days -match "Monday") } | `
+               # Sort-Object "Time Start" | `
+               # Format-Table "Time Start", "Time End", "Class Code"
+
+# $ITSInstructors = $ClassDays | Where-Object { ($_."Class Code" -ilike "SYS*") -or `
+                                              # ($_."Class Code" -ilike "NET*") -or `
+                                              # ($_."Class Code" -ilike "SEC*") -or `
+                                              # ($_."Class Code" -ilike "FOR*") -or `
+                                              # ($_."Class Code" -ilike "CSI*") -or `
+                                              # ($_."Class Code" -ilike "DAT*") } `
+                              # | Select-Object "Instructor" `
+                              # | Sort-Object "Instructor" -Unique
+# $ITSInstructors
+
+$ClassDays | Where { $_.Instructor -in $ITSInstructors.Instructor } `
+           | Group-Object "Instructor" | Select-Object Count, Name | Sort-Object Count -Descending
