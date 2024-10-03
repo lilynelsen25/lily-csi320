5caceeb8115278f8fca8430e7d9a4549696ecddf
Lily <lily.nelsen@mymail.champlain.edu>
2024-10-03 13:56:23 -0400

    First week 5 commit.

---
+++ week5/Lab_5.1.ps1
@@ -0,0 +1,18 @@
+﻿$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.19/ToBeScraped.html
+
+# Get a count of the links in the page 
+$scraped_page.Links.Count
+
+# Display links as HTML Element
+# $scraped_page.Links | Select outerText, href
+
+# Get outer text of every element with the tag h2.
+# $h2s=$scraped_page.ParsedHtml.body.getElementsByTagName("h2") | Select outerText
+
+# $h2s
+
+# Print innerText of every div element that has the class as "div-1"
+# $divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | where {
+# $_.getAttributeNode("class").Value -ilike "div-1"} | Select innerText
+
+# $divs1
